603af342012a095f6789a4ce6466f4f4cdfeaba0
Владислав Плотников <gr436_plval@lab-211-4.tomtit-tomsk.ru>
2026-02-05 09:55:26 +0700

    4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 let app = new Vue({
    el: '#app',
    data: {
-        product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks"
-}
-
+       product: "Socks",
+       image: "./assets/vmSocks-green-onWhite.jpg",
+       altText: "A pair of socks",
+       inventory: 100,
+       inStock: true
+   }
 })
-
-
