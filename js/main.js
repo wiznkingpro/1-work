a8df2e0fd93e628ae91a961fb2ff2fed2a005ba2
Владислав Плотников <gr436_plval@lab-211-4.tomtit-tomsk.ru>
2026-02-05 10:03:50 +0700

    7

--- js/main.js
+++ js/main.js
@@ -6,30 +6,26 @@ let app = new Vue({
        altText: "A pair of socks",
        inStock: true,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-       cart: 0,
        variants: [
-   {
-       variantId: 2234,
-       variantColor: 'green',
-       variantImage: "./assets/vmSocks-green-onWhite.jpg",
+           {
+               variantId: 2234,
+               variantColor: 'green',
+               variantImage: "./assets/vmSocks-green-onWhite.jpg",
+           },
+           {
+               variantId: 2235,
+               variantColor: 'blue',
+               variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+           }
+       ],
+       cart: 0
    },
-   {
-       variantId: 2235,
-       variantColor: 'blue',
-       variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+   methods: {
+       addToCart() {
+           this.cart += 1
+       },
+       updateProduct(variantImage) {
+           this.image = variantImage
+       }
    }
-],
-
-       methods: {
-   addToCart() {
-       this.cart += 1
-   }
-},
-updateProduct(variantImage) {
-   this.image = variantImage
-}
-
-
-   }
-   
 })
