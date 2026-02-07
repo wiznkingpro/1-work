f8f621ef6647d8d60da9871bda7d9a4cec64612a
Владислав Плотников <gr436_plval@lab-211-4.tomtit-tomsk.ru>
2026-02-07 09:32:16 +0700

    11

--- js/main.js
+++ js/main.js
@@ -1,38 +1,120 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div class="product-details">
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+        </div>
+    `
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" :alt="altText"/>
+            </div>
+
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In stock</p>
+                <p v-else>Out of Stock</p>
+                
+                <product-details :details="details"></product-details>
+            <p>Shipping: {{ shipping }}</p>
+                <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+                ></div>
+            </div>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+
+            <button
+                @click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+            
+            
+        </div>
+    `,
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity > 0;
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99;
+            }
+        }
+    }
+})
+
+
 let app = new Vue({
-   el: '#app',
-   data: {
-       product: "Socks",
-       brand: 'Vue Mastery',
-       image: "./assets/vmSocks-green-onWhite.jpg",
-       altText: "A pair of socks",
-       inStock: false,
-       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-       variants: [
-           {
-               variantId: 2234,
-               variantColor: 'green',
-               variantImage: "./assets/vmSocks-green-onWhite.jpg",
-           },
-           {
-               variantId: 2235,
-               variantColor: 'blue',
-               variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-           }
-       ],
-       cart: 0
-   },
-   methods: {
-       addToCart() {
-           this.cart += 1
-       },
-       updateProduct(variantImage) {
-           this.image = variantImage
-       },
-      computed: {
-   title() {
-       return this.brand + ' ' + this.product;
-   }
-}
- 
-   }
+    el: '#app',
+    data: {
+        premium: false
+    }
 })
